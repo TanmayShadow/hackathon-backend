f96c75607728dbec8dd8e8cc16caa9747b1fc66b
TanmayShadow <tanmayshindkar1430@gmail.com>
2025-07-08 01:32:04 +0530

    Update WebConfig

--- src/main/java/com/example/codeforbharat/config/WebClientConfig.java
+++ src/main/java/com/example/codeforbharat/config/WebClientConfig.java
@@ -9,11 +9,6 @@ public class WebClientConfig {
 
     @Bean
     public WebClient webClient() {
-//        return WebClient.builder()
-//                .baseUrl("http://api.kshitijedu.com/ai")
-//                .defaultHeader("X-API-Key", "Qw8nQh1k6n2vKjJ4w9lZp2t8Xy6bQz7rTg5vLm1pQw8nQh1k6n2v")
-//                .defaultHeader("X-Client-Id", "1")
-//                .build();
         return WebClient.builder()
                 .baseUrl("http://127.0.0.1:8000")
                 .defaultHeader("X-API-Key", "Qw8nQh1k6n2vKjJ4w9lZp2t8Xy6bQz7rTg5vLm1pQw8nQh1k6n2v")
